eb4cc30c7fa1d84e2d53206e00e51fb7e991adcf
Mohammad Nhasa <Mohammad.Nhasa@mitrais.com>
2019-10-16 15:37:15 +0700

    Add babelrc file

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Transpile all code following this line with babel and use '@babel/preset-env' (aka ES6) preset.
-require('@babel/register')({
-  presets: ['@babel/preset-env']
-})
+require('@babel/register')
 
 // Import the rest of our application.
 module.exports = require('./server.js')
