26821dd58fc4a25f1bf7cbf387b9336da86eaf72
Mohammad Nhasa <Mohammad.Nhasa@mitrais.com>
2019-10-16 11:30:57 +0700

    Apply standard style

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import express from 'express'
 
 const app = express()
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' })
+})
 
 app.listen(5000)
