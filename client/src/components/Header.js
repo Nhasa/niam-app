66dadaec6f360459336a3335bd9ea9a0e9d3789c
Mohammad Nhasa <Mohammad.Nhasa@mitrais.com>
2019-10-22 14:34:47 +0700

    Add header components

---
+++ client/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Navbar, Nav } from 'react-bootstrap'
+
+const Header = () => (
+  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Customer Books</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="#customers">Customers</Nav.Link>
+        <Nav.Link href="#transations">Transactions</Nav.Link>
+        <Nav.Link href="#mutations">Mutations</Nav.Link>
+      </Nav>
+      <Nav>
+        <Nav.Link href="#logout">
+          Log Out
+      </Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+)
+
+export default Header
