15df16187166c1eb175314c1035637fe52ac8a20
Mohammad Nhasa <Mohammad.Nhasa@mitrais.com>
2019-10-22 14:35:05 +0700

    Introduce auth props

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import Login from './sessions/login'
+import Login from './sessions/Login'
+import Header from './Header'
 import './App.css'
 
-const App = () => {
-  return (
-    <Login />
-  )
-}
+const App = ({ auth }) => auth ? <Header /> : <Login />
 
 export default App
