f00cfb799274b43d014e9ea25581ec7969a3bba7
Mohammad Nhasa <Mohammad.Nhasa@mitrais.com>
2019-10-22 09:51:50 +0700

    Uncomment production flag

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import path from 'path'
 
 const app = express()
 
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
   app.use(express.static('client/build'));
@@ -13,7 +13,7 @@ const app = express()
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-// }
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
