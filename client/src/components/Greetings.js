55f50fc1529a44f899e3ad6b69c352a6e8236f7a
Mohammad Nhasa <Mohammad.Nhasa@mitrais.com>
2019-10-28 13:01:49 +0700

    Add greetings component

---
+++ client/src/components/Greetings.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Jumbotron } from 'react-bootstrap'
+
+const Greetings = () => (
+  <Jumbotron>
+    Please Login / Signup to continue...
+  </Jumbotron>
+)
+
+export default Greetings;
