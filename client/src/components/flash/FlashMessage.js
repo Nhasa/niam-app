7643d27050f1a3e880dac3f5ac29ddc73f09dda2
Mohammad Nhasa <Mohammad.Nhasa@mitrais.com>
2019-10-28 13:03:12 +0700

    Add FlashMessage component

---
+++ client/src/components/flash/FlashMessage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Alert } from 'react-bootstrap'
+
+class FlashMessage extends React.Component {
+  onClick = () => {
+    this.props.deleteFlashMessage(this.props.message.id);
+  }
+
+  render() {
+    const { type, text } = this.props.message;
+    const variant = type === 'error' ? 'danger' : type
+    return (
+      <Alert variant="danger" onClose={this.onClick} dismissible>
+        <Alert.Heading>{variant}</Alert.Heading>
+        <p>
+          {text}
+        </p>
+      </Alert>
+    )
+  }
+}
+
+export default FlashMessage;
