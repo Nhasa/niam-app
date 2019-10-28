522e22a025f9ef9e436d5f6e372a60ebfcffb532
Mohammad Nhasa <Mohammad.Nhasa@mitrais.com>
2019-10-28 13:07:13 +0700

    Implement redux on client

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,37 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import './index.css'
-import App from './components/App'
+import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './rootReducer'
+import setAuthorizationToken from './utils/setAuthorizationToken'
+import routes from './routes'
+// import { setCurrentUser } from './actions/authActions'
+// import jwt from 'jsonwebtoken'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+)
+
+if (localStorage.jwtToken) {
+  setAuthorizationToken(localStorage.jwtToken)
+  // store.dispatch(setCurrentUser(jwt.decode(localStorage.jwtToken)))
+}
+
+render(
+  <Provider store={store}>
+    <BrowserRouter>
+      {routes}
+    </BrowserRouter>
+  </Provider>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
